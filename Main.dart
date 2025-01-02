0791ceafde099f8b1a4e66e8a52747b2c419cd65
Muhammad Bilal khan <155465703+m-bilal-1@users.noreply.github.com>
2025-01-02 18:48:31 +0600

    Create Main.dart
    Page no 1

---
+++ Main.dart
@@ -0,0 +1,14 @@
+//MAin.dart
+import 'package:bmi_calculator/starting%20page.dart';
+import 'package:flutter/material.dart';
+
+import 'bmi.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: one_page()
+    //BMI_Screen(),
+  ));
+}
